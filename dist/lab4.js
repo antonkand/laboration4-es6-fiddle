dba67ce573f37a60ad901ac3fc48e94aec917969
anton <jag@antonkandersson.se>
2014-12-08 21:47:11 +0100

    too tired

--- dist/lab4.js
+++ dist/lab4.js
@@ -4,45 +4,56 @@
   'use strict';
   function Quiz() {
     var $__0 = this;
-    this.getURL = '';
-    this.postURL = '';
+    this.postURL = 'http://vhost3.lnu.se:20080/answer/1';
     this.correctAnswers = [];
-    this.updateQuestion = (function(question) {
-      var addCorrectAnswerToDOM = (function(answer) {
-        $__0.correctAnswers.push(answer);
-        var correctAnswersContainer = document.querySelector('#correct-answers');
-        var li = document.createElement('li');
-        var p = document.createElement('p');
-        p.textContent = answer;
-        li.appendChild(p);
-        correctAnswersContainer.appendChild(li);
-      });
-      var questionContainer = document.querySelector('#question-container');
-      var p = questionContainer.firstChild;
-      if (p.textContent) {
-        addCorrectAnswerToDOM(p.textContent);
-      }
-      p.textContent = question;
+    var domElements = {
+      submit: document.querySelector('#submit'),
+      answer: document.querySelector('#answer'),
+      questionContainer: document.querySelector('#question-container'),
+      correctAnswersContainer: document.querySelector('#correct-answers')
+    };
+    var addCorrectAnswer = (function(answer) {
+      $__0.correctAnswers.push(answer);
+      var li = document.createElement('li');
+      var p = document.createElement('p');
+      p.textContent = answer;
+      li.appendChild(p);
+      domElements.correctAnswersContainer.appendChild(li);
     });
-    this.post = (function(url, answer) {
-      console.log('Quiz.post(url):\n' + url);
-      $__0.getURL = answer;
+    var updateQuestion = (function(question) {
+      var p = document.createElement('p');
+      p.textContent = question;
+      domElements.questionContainer.innerHTML = '';
+      domElements.questionContainer.appendChild(p);
     });
     this.get = (function(url) {
-      console.log('Quiz.get(url):\n' + url);
+      console.log('get url: ' + url);
       var xhr = XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject();
       xhr.open('get', url, false);
-      xhr.addEventListener('load', function() {
+      xhr.addEventListener('load', (function() {
         if (xhr.status < 400) {
-          this.postURL = JSON.parse(xhr.responseText).nextURL;
-          console.log('xhr.status < 400, this.postURL: ' + this.postURL);
-          this.updateQuestion(JSON.parse(xhr.responseText).question);
-          console.log('xhr.status < 400, JSON.parse(xhr.responseText).question: ' + JSON.parse(xhr.responseText).question);
+          $__0.postURL = JSON.parse(xhr.responseText).nextURL;
+          console.log('received postURL: ' + $__0.postURL);
+          updateQuestion(JSON.parse(xhr.responseText).question);
+          console.log('received question: ' + JSON.parse(xhr.responseText).question);
         }
-      });
+      }));
       xhr.send(null);
-      $__0.post('postURL', '42');
     });
+    var post = (function() {
+      var xhr = XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject();
+      xhr.open('post', $__0.postURL);
+      console.log('post url: ' + $__0.postURL);
+      xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+      xhr.onreadystatechange = (function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          alert(xhr.responseText);
+          $__0.get('http://vhost3.lnu.se:20080/question/1');
+        }
+        xhr.send(JSON.stringify({answer: 2}));
+      });
+    });
+    domElements.submit.addEventListener('click', post(), false);
   }
   var run = (function() {
     return new Quiz().get('http://vhost3.lnu.se:20080/question/1');
--- src/lab4.js
+++ src/lab4.js
@@ -1,46 +1,56 @@
 ;(function () {
   'use strict';
   function Quiz () {
-    this.getURL = '';
-    this.postURL = '';
+    this.postURL = 'http://vhost3.lnu.se:20080/answer/1';
     this.correctAnswers = [];
-    this.updateQuestion = (question) => {
-        let addCorrectAnswerToDOM = (answer) => {
-          this.correctAnswers.push(answer);
-          let correctAnswersContainer = document.querySelector('#correct-answers');
-          let li = document.createElement('li');
-          let p = document.createElement('p');
-          p.textContent = answer;
-          li.appendChild(p);
-          correctAnswersContainer.appendChild(li);
-        };
-        let questionContainer = document.querySelector('#question-container');
-        let p = questionContainer.firstChild;
-        if (p.textContent) {
-          addCorrectAnswerToDOM(p.textContent);
-        }
-        p.textContent = question;
+    let domElements = {
+      submit: document.querySelector('#submit'),
+      answer: document.querySelector('#answer'),
+      questionContainer: document.querySelector('#question-container'),
+      correctAnswersContainer: document.querySelector('#correct-answers')
     };
-    this.post = (url, answer) => {
-      console.log('Quiz.post(url):\n' +url);
-      this.getURL = answer;
+    let addCorrectAnswer = (answer) => {
+      this.correctAnswers.push(answer);
+      let li = document.createElement('li');
+      let p = document.createElement('p');
+      p.textContent = answer;
+      li.appendChild(p);
+      domElements.correctAnswersContainer.appendChild(li);
+    };
+    let updateQuestion = (question) => {
+      let p = document.createElement('p');
+      p.textContent = question;
+      domElements.questionContainer.innerHTML = '';
+      domElements.questionContainer.appendChild(p);
     };
     this.get = (url) => {
-      console.log('Quiz.get(url):\n' + url);
+      console.log('get url: ' + url);
       let xhr = XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject();
       xhr.open('get', url, false);
-      xhr.addEventListener('load', function() {
+      xhr.addEventListener('load', () => {
         if (xhr.status < 400) {
           this.postURL = JSON.parse(xhr.responseText).nextURL;
-          console.log('xhr.status < 400, this.postURL: ' + this.postURL);
-          this.updateQuestion(JSON.parse(xhr.responseText).question);
-          console.log('xhr.status < 400, JSON.parse(xhr.responseText).question: ' + JSON.parse(xhr.responseText).question);
+          console.log('received postURL: ' + this.postURL);
+          updateQuestion(JSON.parse(xhr.responseText).question);
+          console.log('received question: ' + JSON.parse(xhr.responseText).question);
         }
       });
       xhr.send(null);
-
-      this.post('postURL', '42');
     };
+    let post = () => {
+      let xhr = XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject();
+      xhr.open('post', this.postURL);
+      console.log('post url: ' + this.postURL);
+      xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+      xhr.onreadystatechange = () => {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+          alert(xhr.responseText);
+          this.get('http://vhost3.lnu.se:20080/question/1');
+        }
+       xhr.send(JSON.stringify({ answer: 2}));
+      };
+    };
+    domElements.submit.addEventListener('click', post(), false);
   }
   let run = () => new Quiz().get('http://vhost3.lnu.se:20080/question/1');
   window.onload = run;
